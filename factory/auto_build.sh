bffeacb835fc4831170e9992630e9115571bfa3d
l <624677295@qq.com>
2023-04-02 22:56:53 +0800

    悠

--- factory/auto_build.sh
+++ factory/auto_build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-Path=/root/Shadowrocket-ADBlock-Rules
+Path=$(pwd)"/.."
+echo $Path
 cd $Path
 
 git fetch --all
 git reset --hard origin/master
 
-cd factory
-python3 ad.py
-python3 gfwlist.py
-python3 build_confs.py
-cd ..
+python3 ./factory/ad.py
+python3 ./factory/gfwlist.py
+python3 ./factory/build_confs.py
+#cd ..
 
 git add .
 git commit -m "Nightly build" -m "已合并最新的去广告规则及 GFWList"
